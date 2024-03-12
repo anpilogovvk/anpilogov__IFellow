b9e36da2e3e8c69294e42b34964553d01db25b7f
Анпилогов Владимир <vbcnthr@gmail.com>
2024-03-12 18:07:36 +0700

    init commit draft branch IF_HW_5

--- src/test/java/hooks/WebHooks.java
+++ src/test/java/hooks/WebHooks.java
@@ -1,20 +1,4 @@
 package hooks;
 
-import com.codeborne.selenide.Browsers;
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
-
-import io.cucumber.java.Before;
-
-
 public class WebHooks {
-
-    @Before
-    public void initBrowser() {
-        Configuration.browser = Browsers.CHROME;
-        Selenide.open("https://edujira.ifellow.ru/login.jsp");
-        WebDriverRunner.getWebDriver().manage().window().maximize();
-    }
-
 }
